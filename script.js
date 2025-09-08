f8b4cc1374915565d68235f5d45cb18d0275d9cd
Proma Dutta <81319416+promadutta11@users.noreply.github.com>
2025-09-08 03:48:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-95% of storage used … If you run out of space, you can't save to Drive, back up Google Photos, or use Gmail. Get 100 GB of storage for $1.99 $0.49/month for 3 months.
 document.addEventListener('DOMContentLoaded', function() {
     // Mobile menu toggle
     const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
@@ -299,3 +298,4 @@ if (contactForm) {
       }
     </style>
   `);
+
